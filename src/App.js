1596a6fc5a223409cb1ad50c4ea4fee8ca15f8d5
Olha Tarasova <tarasovao0505@gmail.com>
2021-09-22 17:36:41 +0300

    Выполнение заданий

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import user from './user.json';
+import statisticalData from './statistical-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json';
+import Profile from './components/Profile';
+import Statistics from './components/Statistics';
+import FriendList from './components/friend-list-item/FriendList';
+import FriendListItem from './components/friend-list-item/FriendListItem';
+import TransactionHistory from './components/TransactionHistory';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={statisticalData} />;
+      <FriendList>
+        {friends.map(friend => (
+          <FriendListItem
+            key={friend.id}
+            id={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        ))}
+      </FriendList>
+      <TransactionHistory items={transactions} />;
     </div>
   );
 }
